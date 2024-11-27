763a6de0823b55304955ea34f2429ca5a6122751
Felipe <www.felipecesargomes@gmail.com>
2024-11-27 20:50:38 -0300

    Commit inicial

---
+++ src/main/java/br/com/felipecesar/tests_junit_mockito/TestsJunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipecesar.tests_junit_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsJunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsJunitMockitoApplication.class, args);
+	}
+
+}
