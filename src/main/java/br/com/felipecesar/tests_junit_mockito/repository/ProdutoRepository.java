e767e7c3756ac2de6394c70cec8125dee1a1068f
Felipe <www.felipecesargomes@gmail.com>
2024-12-05 20:09:44 -0300

    feat:add method find by name in repository

--- src/main/java/br/com/felipecesar/tests_junit_mockito/repository/ProdutoRepository.java
+++ src/main/java/br/com/felipecesar/tests_junit_mockito/repository/ProdutoRepository.java
@@ -4,9 +4,11 @@ import br.com.felipecesar.tests_junit_mockito.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 
-
+    List<Produto> findByNomeContainingIgnoreCase(String nome);
 }
