e9a7d2aeb85f76e7b52b5442efc51ae16082855e
Felipe <www.felipecesargomes@gmail.com>
2024-12-06 15:22:31 -0300

    feat:create Produto DTO

---
+++ src/main/java/br/com/felipecesar/tests_junit_mockito/dto/ProdutoDTO.java
@@ -0,0 +1,36 @@
+package br.com.felipecesar.tests_junit_mockito.dto;
+
+public class ProdutoDTO {
+    private Long id;
+    private String nome;
+    private Double preco;
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Double getPreco() {
+        return preco;
+    }
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
