54f3fbc161813fa177d65630b1ee4341f6d73b00
Felipe <www.felipecesargomes@gmail.com>
2024-12-06 15:18:49 -0300

    feat:create endpoint motor

--- src/main/java/br/com/felipecesar/tests_junit_mockito/controller/HelloController.java
+++ src/main/java/br/com/felipecesar/tests_junit_mockito/controller/HelloController.java
@@ -1,17 +1,36 @@
 package br.com.felipecesar.tests_junit_mockito.controller;
 
+import br.com.felipecesar.tests_junit_mockito.model.Motor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    @Qualifier("motorEletrico")
+    private Motor motor;
+    @Autowired
+    @Qualifier("motorUsado")
+    private Motor motor2;
+
     @GetMapping("/")
     public ResponseEntity<String>
     helloWorld() {
         return ResponseEntity.ok("Hello World");
+    }
+
+    @GetMapping("/motor")
+    public ResponseEntity<List<Motor>>
+    motor() {
+        System.out.println("Motor 1: " + motor + ", Motor 2: " + motor2);
+
+        return ResponseEntity.ok(List.of(motor, motor2));
     }
 }
