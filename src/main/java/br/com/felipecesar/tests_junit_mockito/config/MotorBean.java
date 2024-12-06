edfa9b34d86ff7873a90d21d62b4d14d0c3ffc92
Felipe <www.felipecesargomes@gmail.com>
2024-12-06 15:21:17 -0300

    feat:create motorbean in context spring

---
+++ src/main/java/br/com/felipecesar/tests_junit_mockito/config/MotorBean.java
@@ -0,0 +1,28 @@
+package br.com.felipecesar.tests_junit_mockito.config;
+
+import br.com.felipecesar.tests_junit_mockito.model.Motor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MotorBean {
+
+    @Bean(name = "motorEletrico")
+    public Motor motorEletrico() {
+        Motor motor = new Motor();
+        motor.setLigado(true);
+        motor.setVelocidadeMax(120);
+        motor.setPotencia(220);
+        return motor;
+    }
+
+    @Bean(name = "motorUsado")
+    public Motor motorUsado() {
+        Motor motor = new Motor();
+        motor.setLigado(false);
+        motor.setVelocidadeMax(80);
+        motor.setPotencia(120);
+        return motor;
+    }
+
+}
